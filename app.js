cabbc251446b55a47386408de735b712bd686ba7
Tomas Dato Mgebrisvilis <t.d.mgebrisvilis@gmail.com>
2022-09-12 11:24:42 +0300

    BASIC CRUD DONE

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 // ====================
 // PACKAGES
 // ====================
-const express = require('express'); // require express.
+const express = require('express'); // require express. (npm).
 const app = express(); // asign variable "app" to express().
 const path = require('path'); // require path (node).
-const mongoose = require('mongoose') // require mongoose.
-
+const mongoose = require('mongoose') // require mongoose. (npm).
+const methodOverride = require('method-override'); // require method-override. (npm). (For put delete etc).
 
 // ====================
 // MODELS
 // ====================
 const Campground = require('./models/campground'); // require "Campground" model from ./models/campground.js.
-
 
 // ====================
 // MONGOOSE CONNECTION TO MONGO
@@ -24,14 +23,13 @@ db.once('open', () => { // run when connected.
     console.log('Database connected');
 })
 
-
 // ====================
 // MIDDLEWARE
 // ====================
 app.set('view engine', 'ejs'); // set "view engine" as "ejs" (express, for ejs files).
 app.set('views', path.join(__dirname, 'views')); // set "views" directory (for rendering) to be available from anywhere (express).
 app.use(express.urlencoded({ extended: true })) // parse bodies from urls when there is POST request and where Content-Type header matches type option. (express).
-
+app.use(methodOverride('_method')); // for method-override (put delete etc).
 
 // ====================
 // CRUD: CREATE
@@ -46,7 +44,6 @@ app.post('/campgrounds', async (req, res) => { // post request
     await campground.save(); // save the newly created document.
     res.redirect(`/campgrounds/${campground._id}`); // redirect to this page.
 })
-
 
 // ====================
 // CRUD: READ
@@ -65,6 +62,29 @@ app.get('/campgrounds/:id', async (req, res) => { // get request, page for indiv
     res.render('campgrounds/show', { campground }) // render this file, transfer variable "campgrounds" to it.
 })
 
+// ====================
+// CRUD: EDIT
+// ====================
+app.get('/campgrounds/:id/edit', async (req, res) => { // get request, page with form for editing individual camps.
+    const campground = await Campground.findById(req.params.id) // variable for individual campground (id taken from req.params).
+    res.render('campgrounds/edit', { campground }) // render this file, transfer variable "campgrounds" to it.
+})
+
+app.put('/campgrounds/:id', async (req, res) => { // put request (edit campground).
+    const { id } = req.params; // destructure id from req.params (url).
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }); // update the document, ...SPREAD new updated data to object from req.body.campground.
+    res.redirect(`/campgrounds/${campground._id}`); // redirect to this page.
+})
+
+// ====================
+// CRUD: DELETE
+// ====================
+app.delete('/campgrounds/:id', async (req, res) => { // delete request.
+    const { id } = req.params; // destructure id from req.params (url).
+    await Campground.findByIdAndDelete(id); //  // find by id (from the url) and delete.
+    // console.log(deletedCampground); // show deleted document.
+    res.redirect('/campgrounds'); // redirect to this page.
+})
 
 // ====================
 // CONNECTION TO THE SERVER
