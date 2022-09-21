04dc1b7ae3af8b129c725b0a4bfe4cffd2388481
Tomas Dato Mgebrisvilis <t.d.mgebrisvilis@gmail.com>
2022-09-21 16:21:24 +0300

    small changes

--- utils/ExpressError.js
+++ utils/ExpressError.js
@@ -3,7 +3,7 @@
 // ====================
 /**
  * "ExpressError" is an error handling class that extends regular built-in "Error" error handling class.
- * "ExpressError" will have message & statusCode as properties.
+ * "ExpressError" will have message & statusCode as params.
  * super() is here to access the "Error's" properties and methods.
  * Finally, export this class. 
  */
